8770c73e974184ce9ab064eeba22cd864d9aa0f9
Akira Matsuda <ronnie@dio.jp>
2019-06-04 21:20:06 +0900

    Cross compiled `.so` for this gem may exist only on Windows platforms
    So let's search it only on Windows platforms, and let's not scan through all installed gems on other platforms.

--- lib/io/console.rb
+++ lib/io/console.rb
@@ -1,5 +1,9 @@
-begin
-  require "#{RUBY_VERSION[/\d+\.\d+/]}/io/console.so"
-rescue LoadError
+if Gem.win_platform?
+  begin
+    require "#{RUBY_VERSION[/\d+\.\d+/]}/io/console.so"
+  rescue LoadError
+    require 'io/console.so'
+  end
+else
   require 'io/console.so'
 end
