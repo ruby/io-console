18eb7c1402a26061dd5748349e9d2019f49e6e6c
Charles Oliver Nutter <headius@headius.com>
2023-11-14 11:06:52 -0800

    Move IO.console def to common code
    This should work largely the same across all backends.

--- lib/ffi/io/console/common.rb
+++ lib/ffi/io/console/common.rb
@@ -1,5 +1,44 @@
 # Methods common to all backend impls
 class IO
+  # TODO: Windows version uses "conin$" and "conout$" instead of /dev/tty
+  def self.console(sym = nil, *args)
+    raise TypeError, "expected Symbol, got #{sym.class}" unless sym.nil? || sym.kind_of?(Symbol)
+
+    # klass = self == IO ? File : self
+    if defined?(@console) # using ivar instead of hidden const as in MRI
+      con = @console
+      # MRI checks IO internals : (!RB_TYPE_P(con, T_FILE) || (!(fptr = RFILE(con)->fptr) || GetReadFD(fptr) == -1))
+      if !con.kind_of?(File) || (con.kind_of?(IO) && (con.closed? || !FileTest.readable?(con)))
+        remove_instance_variable :@console
+        con = nil
+      end
+    end
+
+    if sym
+      if sym == :close
+        if con
+          con.close
+          remove_instance_variable :@console if defined?(@console)
+        end
+        return nil
+      end
+    end
+
+    if !con
+      begin
+        con = File.open('/dev/tty', 'r+')
+      rescue
+        return nil
+      end
+
+      con.sync = true
+      @console = con
+    end
+
+    return con.send(sym, *args) if sym
+    return con
+  end
+
   def getch(*, **opts)
     raw(**opts) do
       getc
