1a28f7f5cb452d8889275892ede78ea77447088f
Charles Oliver Nutter <headius@headius.com>
2024-01-14 15:12:30 -0500

    Improve stty console logic
    * Hide stty-specific IO methods
    * Improve and factor out stty command error handling
    * Eliminate verbose syntax warnings

--- lib/ffi/io/console/stty_console.rb
+++ lib/ffi/io/console/stty_console.rb
@@ -9,23 +9,34 @@ warn "io/console on JRuby shells out to stty for most operations" if $VERBOSE
 # Non-Windows assumes stty command is available
 class IO
   if RbConfig::CONFIG['host_os'].downcase =~ /linux/ && File.exists?("/proc/#{Process.pid}/fd")
-    def stty(*args)
-      raise Errno::ENOTTY, inspect if !tty?
-      `stty #{args.join(' ')} < /proc/#{Process.pid}/fd/#{fileno}`
+    protected def _io_console_stty(*args)
+      _io_console_stty_error { `stty #{args.join(' ')} < /proc/#{Process.pid}/fd/#{fileno}` }
     end
   else
-    def stty(*args)
-      raise Errno::ENOTTY, inspect if !tty?
-      `stty #{args.join(' ')}`
+    protected def _io_console_stty(*args)
+      _io_console_stty_error { `stty #{args.join(' ')}` }
     end
   end
 
+  protected def _io_console_stty_error
+    # pre-check to catch non-tty filenos we can't stty against anyway
+    raise Errno::ENOTTY, inspect if !tty?
+
+    result = yield
+
+    case result
+    when /Inappropriate ioctl for device/
+      raise Errno.ENOTTY, inspect
+    end
+
+    result
+  end
+
   def raw(*, min: 1, time: nil, intr: nil)
-    saved = stty('-g')
-    stty('raw')
+    saved = _io_console_stty('-g raw')
     yield self
   ensure
-    stty(saved)
+    _io_console_stty(saved)
   end
 
   def raw!(*)
@@ -33,31 +44,29 @@ class IO
   end
 
   def cooked(*)
-    saved = stty('-g')
-    stty('-raw')
+    saved = _io_console_stty('-g', '-raw')
     yield self
   ensure
-    stty(saved)
+    _io_console_stty(saved)
   end
 
   def cooked!(*)
-    stty('-raw')
+    _io_console_stty('-raw')
   end
 
   def echo=(echo)
-    stty(echo ? 'echo' : '-echo')
+    _io_console_stty(echo ? 'echo' : '-echo')
   end
 
   def echo?
-    (stty('-a') =~ / -echo /) ? false : true
+    (_io_console_stty('-a') =~ / -echo /) ? false : true
   end
 
   def noecho
-    saved = stty('-g')
-    stty('-echo')
+    saved = _io_console_stty('-g', '-echo')
     yield self
   ensure
-    stty(saved)
+    _io_console_stty(saved)
   end
 
   # Not all systems return same format of stty -a output
@@ -65,7 +74,7 @@ class IO
   UBUNTU = 'rows (?<rows>\d+); columns (?<columns>\d+)'
 
   def winsize
-    match = stty('-a').match(/#{IEEE_STD_1003_2}|#{UBUNTU}/)
+    match = _io_console_stty('-a').match(/#{IEEE_STD_1003_2}|#{UBUNTU}/)
     [match[:rows].to_i, match[:columns].to_i]
   end
 
@@ -77,13 +86,13 @@ class IO
       raise ArgumentError.new("wrong number of arguments (given #{sizelen}, expected 2 or 4)")
     end
 
-    row, col, xpixel, ypixel = size
-
     if sizelen == 4
       warn "stty io/console does not support pixel winsize"
     end
 
-    stty("rows #{row} cols #{col}")
+    row, col, _, _ = size
+
+    _io_console_stty("rows #{row} cols #{col}")
   end
 
   def iflush
