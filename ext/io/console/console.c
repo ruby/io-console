f907e5a237d234d9cb72463b8c631f4fe738ad4c
Nobuyoshi Nakada <nobu@ruby-lang.org>
2026-03-15 08:42:56 +0900

    Do not use C99 style comment [ci skip]
    The minimum required ruby version of this library is still 2.6.
    C99 has been adopted since ruby 2.7.

--- ext/io/console/console.c
+++ ext/io/console/console.c
@@ -1710,7 +1710,7 @@ console_dev(int argc, VALUE *argv, VALUE klass)
         Check_Type(sym = argv[0], T_SYMBOL);
     }
 
-    // Force the class to be File.
+    /* Force the class to be File. */
     if (klass == rb_cIO) klass = rb_cFile;
 
     if (console_dev_get(klass, &con)) {
