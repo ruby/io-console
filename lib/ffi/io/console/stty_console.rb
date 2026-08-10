fcb28a3ff7ba5bb3c8229f4b8becc50a7a495820
Nobuyoshi Nakada <nobu@ruby-lang.org>
2026-08-10 13:45:05 +0900

    Improve stty console support
    Use stty with redirected terminals and preserve console modes accurately.

--- lib/ffi/io/console/stty_console.rb
+++ lib/ffi/io/console/stty_console.rb
@@ -3,12 +3,43 @@ module IO::Console
   STTY = %w[/usr/bin/stty /bin/stty].find {|path| File.executable?(path)}
 end
 
-unless IO::Console::STTY &&
-       system(IO::Console::STTY, out: File::NULL, err: %i[child out])
-  raise "stty command returned nonzero exit status"
-end
+raise LoadError, "stty command not found" unless IO::Console::STTY
 
 warn "io/console on JRuby shells out to stty for most operations" if $VERBOSE
+
+class IO::Console::Mode
+  def initialize(saved)
+    @saved = saved
+    @args = []
+  end
+
+  def initialize_copy(mode)
+    super
+    @saved = mode.__send__(:saved).dup
+    @args = mode.__send__(:args).dup
+  end
+
+  def echo=(echo)
+    @args << (echo ? 'echo' : '-echo')
+    echo
+  end
+
+  def raw(min: 1, time: nil, intr: nil)
+    dup.raw!(min:, time:, intr:)
+  end
+
+  def raw!(min: 1, time: nil, intr: nil)
+    @args << 'raw'
+    @args.push('min', min.to_s) if min >= 0
+    @args.push('time', ((time || 0) * 10).to_i.to_s)
+    @args.concat(%w[brkint isig opost]) if intr
+    self
+  end
+
+  private
+
+  attr_reader :saved, :args
+end
 
 # Non-Windows assumes stty command is available
 class IO
@@ -34,11 +65,11 @@ class IO
   end
 
   def raw(*, min: 1, time: nil, intr: nil)
-    saved = _io_console_stty('-g')
-    _io_console_stty('raw')
+    saved = console_mode
+    self.console_mode = saved.raw(min:, time:, intr:)
     yield self
   ensure
-    _io_console_stty(saved) if saved
+    self.console_mode = saved if saved
   end
 
   def raw!(*)
@@ -73,6 +104,15 @@ class IO
     _io_console_stty(saved) if saved
   end
 
+  def console_mode
+    Console::Mode.new(_io_console_stty('-g').chomp)
+  end
+
+  def console_mode=(mode)
+    _io_console_stty(mode.__send__(:saved), *mode.__send__(:args))
+    mode
+  end
+
   # Not all systems return same format of stty -a output
   IEEE_STD_1003_2 = '(?<rows>\d+) rows; (?<columns>\d+) columns'
   UBUNTU = 'rows (?<rows>\d+); columns (?<columns>\d+)'
@@ -96,7 +136,7 @@ class IO
 
     row, col, _, _ = size
 
-    _io_console_stty("rows #{row} cols #{col}")
+    _io_console_stty('rows', row.to_s, 'cols', col.to_s)
   end
 
   def iflush
